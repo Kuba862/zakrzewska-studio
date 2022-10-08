921649a79b1452e2498326b4883e77b08d7854fd
jakubrejch <jrejch@citizengo.net>
2022-10-08 21:07:37 +0200

    init project

---
+++ zakrzewska-cms/config/api.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rest: {
+    prefix: '/v1',
+    defaultLimit: 25,
+    maxLimit: 100,
+    withCount: true,
+  },
+};
---
+++ zakrzewska-react/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+
+  const imagesToken = '61d40877ad21639cbd7e0e67f2de0a36591ee5068ca9e5dad1a5c70b37abef1e600fc8bcf8a17de10f91dc02eb643c2d0d8320dbdbe2e4154aa065ddd95ace364a51127a4600f3c062815867e175b27d8e2b4cb509da72b9c4d7e64b520f6e2208694ca6b51f36b2d598be0039ca7238c6abe15743c6a656c52ad8bf4ca2d436';
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  );
+}
+
+export default App;
